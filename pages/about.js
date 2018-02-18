89f2aa338786585dd7ef248cae161c1a9f1e6571
Anderson Cossul <anderson_cossul@hotmail.com>
2018-02-17 23:47:53 -0200

    First commit (already had created index and about pages).

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Page from '../components/page'
+
+export default () => (
+  <div>
+    <div>
+      <h1>About Page</h1>
+    </div>
+    <style jsx>{`
+    `}</style>
+  </div>
+)
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Page from '../components/page'
+
+const index = () => (
+  <div>
+    <div>
+      <h1>Home Page</h1>
+    </div>
+    <style jsx>{`
+    `}</style>
+  </div>
+)
+
+export default Page(index)
